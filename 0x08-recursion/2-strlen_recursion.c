c7e4ef4f0e5aac7d6f724e0ee5f7b02d049616b3
OnerCode <bsronipahm23@gmail.com>
2023-08-02 11:28:24 -0700

    Corrected function name in 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * _strien_recursion - Returns the length of a string.
+ * _strlen_recursion - Returns the length of a string.
  * @s: the string input
  *
  * Return: the length of the string
  */
 
-int _strien_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 	int word = 0;
 
 	if (*S)
 	{
 		word++;
-		word += _strien_recursion(s + 1);
+		word += _strlen_recursion(s + 1);
 
 	}
 	return (word);

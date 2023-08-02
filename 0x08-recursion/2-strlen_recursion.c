02af355fb762baafd5009f4b98f697058c357fe9
OnerCode <bsronipahm23@gmail.com>
2023-08-02 11:31:46 -0700

    Another correction on 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,22 +1,19 @@
 #include "main.h"
-
 /**
  * _strlen_recursion - Returns the length of a string.
- * @s: the string input
+ * @s: The string to be measured.
  *
- * Return: the length of the string
+ * Return: The length of the string.
  */
-
 int _strlen_recursion(char *s)
 {
-	int word = 0;
+	int longit = 0;
 
-	if (*S)
+	if (*s)
 	{
-		word++;
-		word += _strlen_recursion(s + 1);
-
+		longit++;
+		longit += _strlen_recursion(s + 1);
 	}
-	return (word);
 
+	return (longit);
 }

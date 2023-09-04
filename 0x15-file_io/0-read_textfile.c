03db04a27eae0418261509eb31da7a36e176b370
OnerCode <bsronipahm23@gmail.com>
2023-09-04 12:02:10 -0700

    Completed task #1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile: Read file and print to STDOUT
+ * @filename: file being read
+ * @letters: number of letters read
+ * Return: w- actual number of bytes read and printed
+ *	0 when filename is NULL or function fails
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buff;
+	ssize_t f_op;
+	ssize_t w;
+	ssize_t t;
+
+	f_op = open(filename, O_RDONLY);
+	if (f_op == -1)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	t = read(f_op, buff, letters);
+	w = write(STDOUT_FILENO, buff, t);
+
+	free(buff);
+	close(f_op);
+	return (w);
+}
